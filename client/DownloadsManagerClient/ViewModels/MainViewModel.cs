90080a22d0eb00ac599fa7d4ba8c0f2d00d18c6c
Daxxn <nynjalantz@gmail.com>
2021-09-07 00:26:53 -0700

    Inital Client Commit

---
+++ client/DownloadsManagerClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MVVMLibrary;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace DownloadsManagerClient.ViewModels
+{
+   public class MainViewModel : Model
+   {
+      #region - Fields & Properties
+
+      #region - Commands
+
+      #endregion
+      #endregion
+
+      #region - Constructors
+      public MainViewModel() { }
+      #endregion
+
+      #region - Methods
+
+      #region - Command Methods
+
+      #endregion
+      #endregion
+
+      #region - Full Properties
+
+      #endregion
+   }
+}
